a84b14c19f361af42ad1a45b70c5fff360f14389
Germán "amorcito" Izquierdo <izquierdoz.g@gmail.com>
2022-12-01 16:52:45 +0000

    style fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ script that starts a Flask web application """
+
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,5 +11,7 @@ def hello():
     """ Prints when going in the web app """
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
